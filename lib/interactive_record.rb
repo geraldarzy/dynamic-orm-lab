6f83d6e03e73dd5ccfc5c196f7ae4184e691046d
geraldarzy <geraldarzola@gmail.com>
2021-01-20 04:19:25 -0500

    commit tree e7162a561641edd96a36913ca6d814616cb33e23

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,56 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    DB[:conn].results_as_hash = true
+    sql = "PRAGMA table_info('#{table_name}')"
+    table_info = DB[:conn].execute(sql)
+    col_names = []
+    table_info.each do |col|
+        col_names << col['name']
+    end
+    col_names.compact
+  end
+
+  def initialize (options = {})
+    options.each do |x,y|
+        self.send("#{x}=",y)
+    end
+  end
+
+  def table_name_for_insert
+    self.class.to_s.downcase.pluralize
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col|col == 'id'}.join(', ')
+    #returns column names, csv, no id
+  end
+
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+        values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(', ')
+    #returns values of col name, csv, no id
+  end
+
+  def save
+    DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert});")
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+    end
+
+  def find_by_name(name)
+    
+    DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name = ? LIMIT 1",name)
+  end
+
   
 end
